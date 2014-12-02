1a83042a07bc3a8c1898ceab1f81b9c2fca29077
cpradio <forums@cpradio.net>
2014-12-02 09:22:59 -0500

    Clean up the stylesheets

--- assets/javascripts/composerHelp.js
+++ assets/javascripts/composerHelp.js
@@ -1,6 +1,5 @@
 Discourse.ComposerHelpContentView = Discourse.View.extend({
     tagName: "div",
-    classNames: ["composer-help-content"],
     parsedContent: function() {
         return this.get("model.content");
     }.property("model.content")
