7573df0744c78b814e4a40f62330292f0bf89df3
cpradio <forums@cpradio.net>
2015-04-24 10:20:39 -0400

    Update version to 0.2 Rename composer-help.js.handlebars to composer-help.hbs

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,10 @@
 # name: composer-help-button
 # about: Add a help button to the composer window
-# version: 0.1
+# version: 0.2
 # authors: Matthew Wilkin
 
 
 register_asset "javascripts/admin/templates/site_settings/setting_text.hbs"
-register_asset "javascripts/discourse/templates/composer-help.js.handlebars"
+register_asset "javascripts/discourse/templates/composer-help.hbs"
 
 register_asset 'stylesheets/composer-help.scss'
